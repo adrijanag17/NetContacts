de71521f246a38933ea24a418cd8e9b32965ae42
Adrija <71489040+adrijanag17@users.noreply.github.com>
2023-08-09 00:30:35 -0700

    Create entity class representing person info

---
+++ src/main/java/com/example/adrija/springapi/Person.java
@@ -0,0 +1,64 @@
+package com.example.adrija.springapi;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String org;
+    private String role;
+    private String via;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setOrg(String org) {
+        this.org = org;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setVia(String via) {
+        this.via = via;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getOrg() {
+        return org;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getVia() {
+        return via;
+    }
+
+}
