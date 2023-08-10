3568ca909dd4efe9e37639635d7029cc6519a203
Adrija <71489040+adrijanag17@users.noreply.github.com>
2023-08-10 00:44:05 -0700

    Add contact delete functionality

--- contacts-frontend/src/components/ContactCard.jsx
+++ contacts-frontend/src/components/ContactCard.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import { TrashIcon, PencilSquareIcon } from '@heroicons/react/20/solid';
+import axios from 'axios';
+
 
 const ContactCard = ({ contact }) => {
+
+    const handleDelete = () => {
+        axios.delete(`http://localhost:8080/contacts/${contact.id}`)
+          .then(response => {
+            console.log('Contact deleted successfully:', response.data);
+          })
+          .catch(error => {
+            console.error('Error deleting contact:', error);
+          });
+    };
+
   return (
     <div className="bg-white rounded-lg p-4 shadow-md flex flex-col justify-evenly items-center h-full">
         <h2 className="text-lg font-semibold">{contact.name}</h2>
@@ -9,6 +23,14 @@ const ContactCard = ({ contact }) => {
         <p className="text-gray-500">{contact.org}</p>
         <p className="text-gray-500">{contact.role}</p>
         <p className="text-gray-500">{contact.via}</p>
+        <div className="mt-4 flex space-x-10">
+            <button onClick={handleDelete} className="text-red-500 hover:text-red-600">
+              <TrashIcon className="h-5 w-5" />
+            </button>
+            <button className="text-blue-500 hover:text-blue-600">
+              <PencilSquareIcon className="h-5 w-5" />
+            </button>
+        </div>
     </div>
   );
 };
