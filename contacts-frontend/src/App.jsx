988bf869a50a0d144a7ef8c84ccad33824e2fa99
Adrija <71489040+adrijanag17@users.noreply.github.com>
2023-08-10 02:07:47 -0700

    Add contact edit functionality

--- contacts-frontend/src/App.jsx
+++ contacts-frontend/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
 
     const [contacts, setContacts] = useState([]);
     const [isEditCardVisible, setIsEditCardVisible] = useState(false);
+    const [editingContact, setEditingContact] = useState(null);
 
 
     useEffect(() => {
@@ -29,15 +30,44 @@ function App() {
         setContacts(prevContacts => prevContacts.filter(contact => contact.id !== deletedId));
     };
 
+    const handleEditContact = (contact) => {
+        setEditingContact(contact);
+        setIsEditCardVisible(true);
+    };
+
+//     const handleUpdateContact = (updatedContact) => {
+//         // Find the id of the updated contact
+//         const updatedIndex = contacts.findIndex((contact) => contact.id === updatedContact.id);
+//
+//         if (updatedIndex !== -1) {
+//           // Update the contacts array with the updated contact
+//           setContacts((prevContacts) => [
+//             ...prevContacts.slice(0, updatedIndex),
+//             updatedContact,
+//             ...prevContacts.slice(updatedIndex + 1),
+//           ]);
+//         }
+//     };
+
+    const handleUpdateContact = (updatedContact) => {
+      const updatedContacts = contacts.map((contact) =>
+        contact.id === updatedContact.id ? updatedContact : contact
+      );
+
+      setContacts(updatedContacts);
+    };
+
   return (
     <div className="min-h-screen bg-gray-100 flex justify-center items-center">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 p-10 w-full max-w-screen-lg mx-auto">
         {contacts.map(contact => (
           <ContactCard key={contact.id} contact={contact} onDelete={handleDeleteContact}
-          onEdit={() => setIsEditCardVisible(true)}/>
+          onEdit={() => handleEditContact(contact)}/>
         ))}
       </div>
-      {isEditCardVisible && <EditCard setIsEditCardVisible={setIsEditCardVisible} />}
+      {isEditCardVisible && <EditCard contact={editingContact}
+                                      setIsEditCardVisible={setIsEditCardVisible}
+                                      updateContact={handleUpdateContact}/>}
     </div>
   );
 }
--- contacts-frontend/src/components/EditCard.jsx
+++ contacts-frontend/src/components/EditCard.jsx
@@ -1,16 +1,42 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const EditCard = ({ setIsEditCardVisible }) => {
+const EditCard = ({ contact, setIsEditCardVisible, updateContact }) => {
+    const [editedContact, setEditedContact] = useState(contact);
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setEditedContact((prevContact) => ({ ...prevContact, [name]: value }));
+    };
+
+    const handleSave = () => {
+        axios
+          .put(`http://localhost:8080/contacts/${contact.id}`, editedContact)
+          .then((response) => {
+            updateContact(editedContact); // Update the contact in the parent component's state
+            setIsEditCardVisible(false); // Close the edit card
+            console.log('Contact updated successfully:', response.data);
+          })
+          .catch((error) => {
+            console.error('Error updating contact:', error);
+          });
+    };
+
+
   return (
     <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center">
       <div className="bg-white rounded-lg p-4 shadow-md w-full max-w-md">
         <h2 className="text-lg font-semibold mb-4">Edit contact details</h2>
-        <input type="text" className="border rounded-lg px-3 py-2 mb-2 w-full" placeholder="Name" />
-        <input type="email" className="border rounded-lg px-3 py-2 mb-2 w-full" placeholder="Email" />
-        <input type="text" className="border rounded-lg px-3 py-2 mb-2 w-full" placeholder="Organization" />
-        <input type="text" className="border rounded-lg px-3 py-2 mb-2 w-full" placeholder="Role" />
-        <input type="text" className="border rounded-lg px-3 py-2 mb-2 w-full" placeholder="Connection via" />
+        <input type="text" className="border rounded-lg px-3 py-2 mb-2 w-full" placeholder="Name"
+            name="name" value={editedContact.name} onChange={handleInputChange} />
+        <input type="email" className="border rounded-lg px-3 py-2 mb-2 w-full" placeholder="Email"
+            name="email" value={editedContact.email} onChange={handleInputChange}/>
+        <input type="text" className="border rounded-lg px-3 py-2 mb-2 w-full" placeholder="Organization"
+            name="org" value={editedContact.org} onChange={handleInputChange}/>
+        <input type="text" className="border rounded-lg px-3 py-2 mb-2 w-full" placeholder="Role"
+            name="role" value={editedContact.role} onChange={handleInputChange}/>
+        <input type="text" className="border rounded-lg px-3 py-2 mb-2 w-full" placeholder="Connection via"
+            name="via" value={editedContact.via} onChange={handleInputChange}/>
 
         <div className="flex justify-end">
           <button className="text-gray-500 hover:text-gray-600 mr-2"
@@ -18,7 +44,8 @@ const EditCard = ({ setIsEditCardVisible }) => {
             Cancel
           </button>
           <button
-            className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">
+            className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+            onClick={handleSave}>
             Save
           </button>
         </div>
