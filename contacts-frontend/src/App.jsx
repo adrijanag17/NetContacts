bb6844b1f3d72c6061e4563ff01b13ec4334a26b
Adrija <71489040+adrijanag17@users.noreply.github.com>
2023-09-05 03:23:58 -0700

    Refactor code

--- contacts-frontend/src/App.jsx
+++ contacts-frontend/src/App.jsx
@@ -1,105 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import ContactCard from './components/ContactCard';
-import EditCard from './components/EditCard';
-import Header from './components/Header';
-import { PlusIcon, MagnifyingGlassIcon } from '@heroicons/react/24/solid';
-
-
+import React from 'react';
+import ContactPage from './components/ContactPage';
 function App() {
 
-    const [contacts, setContacts] = useState([]);
-    const [isEditCardVisible, setIsEditCardVisible] = useState(false);
-    const [editingContact, setEditingContact] = useState(null);
-    const [searchQuery, setSearchQuery] = useState('');
-    const [filteredContacts, setFilteredContacts] = useState([]);
-
-    useEffect(() => {
-        // Fetch data from backend API
-        axios.get('http://localhost:8080/contacts')
-        .then(response => {
-            // Update the contacts array with fetched data
-            setContacts(response.data);
-            setFilteredContacts(response.data);
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
-    }, []);
-
-    const handleDeleteContact = (deletedId) => {
-        // Remove the deleted contact from the contacts array
-        setContacts(prevContacts => prevContacts.filter(contact => contact.id !== deletedId));
-    };
-
-    const handleEditContact = (contact) => {
-        setEditingContact(contact);
-        setIsEditCardVisible(true);
-    };
-
-    const handleUpdateContact = (updatedContact) => {
-      const updatedContacts = contacts.map((contact) =>
-        contact.id === updatedContact.id ? updatedContact : contact
-      );
-
-      setContacts(updatedContacts);
-    };
-
-    const handleAddContact = (newContact) => {
-      setContacts((prevContacts) => [...prevContacts, newContact]);
-    };
-
-    const handleSearch = (event) => {
-        const query = event.target.value;
-        setSearchQuery(query);
-
-        // Filter contacts based on the search query
-        const filtered = contacts.filter((contact) =>
-          contact.name.toLowerCase().includes(query.toLowerCase())
-        );
-
-        // Update the filteredContacts state with the filtered results
-        setFilteredContacts(filtered);
-      };
-
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-200 to-pink-200">
-      <Header />
-      <div className="p-10 max-w-screen-lg mx-auto">
-          <div className="relative mx-4 mb-10">
-            <span className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                <MagnifyingGlassIcon className="w-5 h-5 text-gray-400" />
-            </span>
-            <input
-                type="text"
-                placeholder="Search contacts..."
-                value={searchQuery}
-                onChange={handleSearch}
-                className="w-full pl-10 pr-4 py-2 border rounded-full focus:outline-none focus:ring focus:border-blue-300"
-            />
-          </div>
-
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
-            {filteredContacts.map((contact) => (
-              <ContactCard
-                key={contact.id}
-                contact={contact}
-                onDelete={handleDeleteContact}
-                onEdit={() => handleEditContact(contact)}
-              />
-            ))}
-          </div>
-      </div>
-      <button
-        className="fixed bottom-10 right-10 bg-blue-500 text-white p-3 rounded-full shadow-lg hover:bg-blue-600"
-        onClick={() => setIsEditCardVisible(true)}>
-        <PlusIcon className="h-5 w-5" />
-      </button>
-      {isEditCardVisible && <EditCard contact={editingContact}
-                                                setIsEditCardVisible={setIsEditCardVisible}
-                                                updateContact={handleUpdateContact}
-                                                addContact={handleAddContact}/>}
-
+    <div>
+        <ContactPage />
     </div>
   );
 }
--- contacts-frontend/src/components/Header.jsx
+++ contacts-frontend/src/components/Header.jsx
@@ -1,27 +1,6 @@
 import React from 'react';
 import logo from '../../public/handshake.png'
 
-// const Header = () => {
-//   return (
-//     <header className="bg-gray-100 py-4 text-gray-800 text-center">
-//       <h1 className="text-2xl font-semibold">Your Networking Contacts</h1>
-//     </header>
-//   );
-// };
-//
-// export default Header;
-// const Header = () => {
-//   return (
-//     <header className="py-6 text-gray-800">
-//       <div className="container mx-auto text-center">
-//         <img src={logo} alt="Handshake Logo" className="w-20 h-20 rounded-full"/>
-//         <h1 className="text-4xl font-semibold mb-2">Your Networking Contacts</h1>
-//         <p className="text-lg text-gray-600">Stay connected with your professional network.</p>
-//       </div>
-//     </header>
-//   );
-// };
-
 const Header = () => {
   return (
     <header className="py-6 text-gray-800">
